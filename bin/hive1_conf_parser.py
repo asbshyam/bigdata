edf51b1fefcf90d436fc83ad45c3537b49231359
asbshyam <asbshyam@gmail.com>
2016-10-12 18:08:15 +0800

    Create hive1_conf_parser.py

---
+++ bin/hive1_conf_parser.py
@@ -0,0 +1,51 @@
+import os
+from xml.etree import ElementTree
+
+for filename in os.listdir('/data/2/gpabdadmin/tmp/mr_config/dev'):
+	with open('/data/2/gpabdadmin/tmp/mr_config/dev/' + filename, 'rt') as f:
+		tree = ElementTree.parse(f)
+
+	nameline = line = ""
+	n = 1
+	output = oozie = hive = names = False
+	filter1 = ["mapreduce.job.user.name","hive.server2.authentication","oozie.launcher.action.main.class"]
+
+	for node in tree.iter():
+	
+		if node.tag == "name"  and node.text in filter1:
+		 # line = line + "NAME : " + node.text
+		 output = True
+		 if node.text == "oozie.launcher.action.main.class":
+		  oozie =  True
+		 elif node.text == "mapreduce.job.user.name":
+		  names = True
+		 elif node.text == "hive.server2.authentication":
+		  hive = True
+		  
+		if node.tag == "value" and output:
+		 if oozie and node.text == "org.apache.oozie.action.hadoop.HiveMain":
+		  # line = line + " OOZIE ACTION # " + node.text + "\n"
+		  line = line + node.text
+		 elif names:
+		  # nameline = " NAME         # " + node.text
+		  nameline = node.text
+		  names = False
+		 elif hive:
+		  hive = True
+		 else:
+		  hive = oozie = names = output = False
+		  
+		if node.tag == "source" and output and hive:
+		 if("/var/run" not in node.text and "/data/" not in node.text and "job.xml" not in node.text and "programatically" not in node.text and "hive-site.xml" != node.text):
+			 # line = line + " HIVE1        # " + str(n) + ": " + node.text + "\n"	
+			 line = line + node.text
+			 n = n + 1
+		 
+		if node.tag == "property" and output:
+		 hive = oozie = names = output = False
+		 
+	if line != "":
+	 print( filename + "," + line + "," + nameline )
+	 line =  ""
+	 n = 1
+	 
