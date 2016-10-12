c5ea0079d8fc22142a31c0b75a44c00b8549f102
asbshyam <asbshyam@gmail.com>
2016-10-12 18:38:24 +0800

    Update FindActiveRM.py

--- bin/FindActiveRM.py
+++ bin/FindActiveRM.py
@@ -7,13 +7,12 @@ flag = False
 rm = []
 for line in file:
     if flag:
-        rm.append(line[line.find(">")+1:line.find("</value>")])
+        rm.append("http://" + line[line.find(">")+1:line.find("</value>")])
         flag = False
     if re.search("yarn.resourcemanager.webapp.address", line):
         flag = True
 
 for c in rm:
-        c = "http://" + c
         r = requests.get(c, data={'key': 'value'})
         message = "This is standby RM. Redirecting to the current active RM"
         if message in str(r.content):
